239e921d8322017b83aee75e879d814640c88745
in64 <admin@in64.local>
2024-08-25 16:00:38 +0800

    update prerelease

--- install_release.sh
+++ install_release.sh
@@ -3,7 +3,7 @@ set -e
 
 BUILDDIR=build-auto
 PREBUILT_SERVER_URL=https://pub.seiya.dev/scrcpy-server
-PREBUILT_SERVER_SHA256=a9363c44b1f863867a8a24e23e12909d7b0052173ccbdfefff65f69af0ebd086
+PREBUILT_SERVER_SHA256=934c786d9964261021f0e8ba77da6362625c50a526b5c322b78de71359ba47e6
 
 echo "[scrcpy] Downloading prebuilt server..."
 wget "$PREBUILT_SERVER_URL" -O scrcpy-server
